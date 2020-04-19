7dfe449b00e7fce91a52cddc022b2dc93aff78f3
drayanu <diegoraya000@gmail.com>
2020-04-19 17:43:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-yg1csk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-yg1csk)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Qué lección nos dan Cantabria sobre la calidad de vida</h1>`;
