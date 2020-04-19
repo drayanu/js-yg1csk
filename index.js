3eef4a03b7634685cda21afa92b69372ff3e7c6f
drayanu <diegoraya000@gmail.com>
2020-04-19 20:43:29 +0200

    2 files modified

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Qué lección nos dan Cantabria sobre la calidad de vida</h1>`;
+appDiv.innerHTML = `<h1>Qué lección nos da Cantabria sobre la calidad de vida</h1>`;
